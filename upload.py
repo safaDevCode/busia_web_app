efa90b88f3cccd1124187cf43c713311c6767a6f
safadevcode <daniel.obare@invent.co.ke>
2025-05-21 09:29:16 +0300

    changed title to Busia

--- upload.py
+++ upload.py
@@ -153,7 +153,7 @@ def data_page():
         st.rerun()
         return
 
-    st.title("Bungoma Data Management")
+    st.title("Busia Data Management")
     st.write(f"Welcome, {st.session_state['username']}! (Department: {st.session_state['department']})")
 
     if st.button("Logout"):
